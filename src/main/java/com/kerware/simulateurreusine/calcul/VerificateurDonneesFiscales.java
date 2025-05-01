75ef4eedc2fd6eaa84a7f5d124d1bed475d5e97a
Léo-Paul <marie2328@unicaen.fr>
2025-05-01 17:44:14 +0200

    :hammer: Individualize data validator logic

--- src/main/java/com/kerware/simulateurreusine/calcul/VerificateurDonneesFiscales.java
+++ src/main/java/com/kerware/simulateurreusine/calcul/VerificateurDonneesFiscales.java
@@ -19,99 +19,53 @@ public class VerificateurDonneesFiscales {
             int nbEnfantsHandicapes,
             boolean estparentIsole
     ) {
-        if (revenuNetDeclarant1 < REVENU_MIN || revenuNetDeclarant2 < REVENU_MIN) {
-            throw new IllegalArgumentException("Le revenu net ne peut pas être négatif");
+        String erreur = "";
+        if (revenuNetDeclarant1 < REVENU_MIN
+                || revenuNetDeclarant2 < REVENU_MIN) {
+            erreur = "Le revenu net ne peut pas être négatif";
+            throw new IllegalArgumentException(erreur);
         }
 
         if (nbEnfants < NB_ENFANTS_MIN) {
-            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être négatif");
+            erreur = "Le nombre d'enfants ne peut pas être négatif";
+            throw new IllegalArgumentException(erreur);
         }
 
         if (nbEnfantsHandicapes < NB_ENF_HANDICAPES_MIN) {
-            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être négatif");
+            erreur = "Le nombre d'enfants handicapés ne peut pas être négatif";
+            throw new IllegalArgumentException(erreur);
         }
 
         if (situationFamiliale == null) {
-            throw new IllegalArgumentException("La situation familiale ne peut pas être null");
+            erreur = "La situation familiale ne peut pas être null";
+            throw new IllegalArgumentException(erreur);
         }
 
         if (nbEnfantsHandicapes > nbEnfants) {
-            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être supérieur au nombre d'enfants");
+            erreur = "Le nombre d'enfants handicapés" +
+                    " ne peut pas être supérieur au nombre d'enfants";
+            throw new IllegalArgumentException(erreur);
         }
 
         if (nbEnfants > NB_ENFANTS_MAX) {
-            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être supérieur à 7");
+            erreur = "Le nombre d'enfants ne peut pas être supérieur à 7";
+            throw new IllegalArgumentException(erreur);
         }
 
-        if (estparentIsole && (situationFamiliale == SituationFamiliale.MARIE || situationFamiliale == SituationFamiliale.PACSE)) {
-            throw new IllegalArgumentException("Un parent isolé ne peut pas être marié ou pacsé");
+        if (estparentIsole && (situationFamiliale == SituationFamiliale.MARIE
+                || situationFamiliale == SituationFamiliale.PACSE)) {
+            erreur = "Un parent isolé ne peut pas être marié ou pacsé";
+            throw new IllegalArgumentException(erreur);
         }
 
-        boolean seul = situationFamiliale == SituationFamiliale.CELIBATAIRE || situationFamiliale == SituationFamiliale.DIVORCE || situationFamiliale == SituationFamiliale.VEUF;
-        if (seul && revenuNetDeclarant2 > REVENU_AUTORISE_DECLARANT2_SI_SEUL) {
-            throw new IllegalArgumentException("Un célibataire, un divorcé ou un veuf ne peut pas avoir de revenu pour le déclarant 2");
-        }
-    }
-
-    /*
-    private int revenuNetDeclarantMinimum;
-    private int nbEnfantsMinimum;
-    private int nbEnfantsMaximum;
-    private int nbEnfantsHandicapesMinimum;
-
-    public VerificateurDonneesFiscales(
-            int paramRevenuNetDeclarantMinimum,
-            int paramNbEnfantsMinimum,
-            int paramNbEnfantsMaximum,
-            int paramNbEnfantsHandicapesMinimum) {
-        this.revenuNetDeclarantMinimum = paramRevenuNetDeclarantMinimum;
-        this.nbEnfantsMinimum = paramNbEnfantsMinimum;
-        this.nbEnfantsMaximum = paramNbEnfantsMaximum;
-        this.nbEnfantsHandicapesMinimum = paramNbEnfantsHandicapesMinimum;
-    }
-
-    public void verifierDonnees(
-            int revenuNetDeclarant1,
-            int revenuNetDeclarant2,
-            SituationFamiliale situationFamiliale,
-            int nbEnfants,
-            int nbEnfantsHandicapes,
-            boolean estparentIsole
-    ) {
-        if (revenuNetDeclarant1 < this.revenuNetDeclarantMinimum || revenuNetDeclarant2 < this.revenuNetDeclarantMinimum) {
-            throw new IllegalArgumentException("Le revenu net ne peut pas être négatif");
-        }
-
-        if (nbEnfants < this.nbEnfantsMinimum) {
-            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être négatif");
-        }
-
-        if (nbEnfantsHandicapes < this.nbEnfantsHandicapesMinimum) {
-            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être négatif");
-        }
-
-        if (situationFamiliale == null) {
-            throw new IllegalArgumentException("La situation familiale ne peut pas être null");
-        }
-
-        if (nbEnfantsHandicapes > nbEnfants) {
-            throw new IllegalArgumentException("Le nombre d'enfants handicapés ne peut pas être supérieur au nombre d'enfants");
-        }
-
-        if (nbEnfants > this.nbEnfantsMaximum) {
-            throw new IllegalArgumentException("Le nombre d'enfants ne peut pas être supérieur à 7");
-        }
-
-        if (estparentIsole && (situationFamiliale == SituationFamiliale.MARIE || situationFamiliale == SituationFamiliale.PACSE)) {
-            throw new IllegalArgumentException("Un parent isolé ne peut pas être marié ou pacsé");
-        }
-
-        boolean seul = situationFamiliale == SituationFamiliale.CELIBATAIRE || situationFamiliale == SituationFamiliale.DIVORCE || situationFamiliale == SituationFamiliale.VEUF;
-        if (seul && revenuNetDeclarant2 > 0) {
-            throw new IllegalArgumentException("Un célibataire, un divorcé ou un veuf ne peut pas avoir de revenu pour le déclarant 2");
+        boolean seul = situationFamiliale == SituationFamiliale.CELIBATAIRE
+                || situationFamiliale == SituationFamiliale.DIVORCE
+                || situationFamiliale == SituationFamiliale.VEUF;
+        if (seul &&
+                revenuNetDeclarant2 > REVENU_AUTORISE_DECLARANT2_SI_SEUL) {
+            erreur = "Un célibataire, un divorcé ou un veuf" +
+                    " ne peut pas avoir de revenu pour le déclarant 2";
+            throw new IllegalArgumentException(erreur);
         }
     }
-
-    */
-
 }
